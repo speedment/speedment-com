6d71cc56f172b66051d6446b2b6b59a76d2be97b
pyknic <emil@speedment.com>
2017-03-27 14:00:38 -0700

    Create Speedment 2017 theme

---
+++ wp-content/themes/speedment-2017/index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <meta name="keywords" content="Java,Database,Stream,Pure,ORM">
+    <meta name="author" content="Speedment, Inc.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name'); ?></title>
+  </head>
+  <body>
+    <h1>Say hello to the new <?php bloginfo('name'); ?></h1>
+    <p>
+        <?php bloginfo('description'); ?>
+    </p>
+  </body>
+</html>
