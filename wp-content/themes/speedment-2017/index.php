a0fcc3868d9d300c458ed2bb7da75381a26d5c2f
pyknic <emil@speedment.com>
2017-03-27 20:25:45 -0700

    Add FontAwesome dependency

--- wp-content/themes/speedment-2017/index.php
+++ wp-content/themes/speedment-2017/index.php
@@ -18,79 +18,82 @@
     ); ?>
 </nav>
 
+<h3>Top right menu</h3>
+<?php wp_nav_menu(array('menu'=>'top-right-menu'));?>
+
+<h3>Bottom left menu</h3>
+<?php wp_nav_menu(array('menu'=>'bottom-left-menu'));?>
+
+<h3>Bottom center menu</h3>
+<?php wp_nav_menu(array('menu'=>'bottom-center-menu'));?>
+
+<h3>Bottom right menu</h3>
+<?php wp_nav_menu(array('menu'=>'bottom-right-menu'));?>
+
+<i class="fa fa-book" style="font-size:10000%"></i>
+
 
 <!-- Main hero unit for a primary marketing message or call to action -->
-  <div class="jumbotron jumbotron-fluid" id="first-view">
-  <div class="row justify-content-md-center">
-    <div class="col-7 pull-2 col-md-auto">
-      <div class="col-md-auto" id="first-view-text-wrapper">
-        <h4 class="d-inline-flex"># Tool to Accelereate Existing <br> Database Applications</h4>
-        <form class="lead d-inline-flex">
-          <label class="sr-only" for="inlineFormInput">Name</label>
-          <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0"
-          id="inlineFormInput" placeholder="Enter your work email">
-          <button type="submit" class="btn btn-success">Try For Free</button>
-        </form>
-     </div>
-    </div>
-</div>
+<<<<<<< Updated upstream
+<div class="hero-unit">
+  <h1>Hello, <?php bloginfo('name'); ?>!</h1>
+  <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+  <p><a class="btn btn-primary btn-large">Learn more &raquo;</a></p>
 </div>
 
-
-
-
-<div class="container-fluid">
-  <div id="info-one">
-    Speedment enables you to perform database actions using Java 8
-    <div class="row">
-      <div class="col-sm-4">
-        <h4>Open Source</h4>
-      </div>
-      <div class="col-sm-4">
-        <h4>Enterprise</h4>
-      </div>
-      <div class="col-sm-4">
-        <h4>Customized</h4>
+  <nav class="navbar navbar-toggleable-md navbar-light bg-faded" id="developer-navbar">
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="#">Features</a>
+        <a class="nav-item nav-link" href="#">Pricing</a>
+        <a class="nav-item nav-link disabled" href="#">Disabled</a>
       </div>
     </div>
+  </nav>
+  <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Navbar</a>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="#">Features</a>
+        <a class="nav-item nav-link" href="#">Pricing</a>
+        <a class="nav-item nav-link disabled" href="#">Disabled</a>
+      </div>
+    </div>
+  </nav>
+=======
+>>>>>>> Stashed changes
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-3"># Tool to Accelereate Existing <br> Database Applications</h1>
+    <p class="lead"><form><input type="text"/> <input type="button"/></form></p>
   </div>
-  <div class="row justify-content-md-center" id="why-speedment">
-    <div class="col-sm-3">
-      <div class="text-center">
-          <img src="http://www.iconsdb.com/icons/preview/white/speedometer-xxl.png" class="rounded" alt="Speed">
-      </div>
-      <h4>It´s Pure Java</h4>
-      <span>Single language in the entire applicaton. No need to learn SQL or an ORM API.</span>
+</div>
+<div class="row" id="why-speedment">
+  <div class="col-sm-4">
+    <div class="text-center">
+        <img src="http://icons.iconarchive.com/icons/icons8/android/512/Measurement-Units-Speed-icon.png" class="rounded" alt="Speed">
     </div>
-    <div class="col-sm-3">
-      <div class="text-center">
-        <img src="http://www.iconsdb.com/icons/preview/white/speedometer-xxl.png" class="rounded" alt="Speed">
-
-      </div>
-      <h4>Super Fast</h4>
-      <span>Single language in the entire applicaton. No need to learn SQL or an ORM API.</span>
-    </div>
-    <div class="col-sm-3">
-      <div class="text-center">
-        <img src="http://www.iconsdb.com/icons/preview/white/speedometer-xxl.png" class="rounded" alt="Speed">
-      </div>
-      <h4>Run Anywhere</h4>
-      <span>Single language in the entire applicaton. No need to learn SQL or an ORM API.</span>
-    </div>
+    <h4>It´s Pure Java</h4>
+    <span>Single language in the entire applicaton. No need to learn SQL or an ORM API.</span>
   </div>
-  <div class="row information justify-content-md-center">
-    <div class="col-sm-5">
-      <h3>Guides</h3>
-      <p>Whether you´r an export or newcomer our task-focused Gettin Started. Guides and tutorials are designed to get your productive with Spring as Quckly as possible.</p>
-      <a class="offset-feature--link" href="/guides">
-          Browse the Guides
-      <i class="icon-chevron-right"></i>
+  <div class="col-sm-4">
+    <div class="text-center">
+        <img src="http://icons.iconarchive.com/icons/icons8/android/512/Measurement-Units-Speed-icon.png" class="rounded" alt="Speed">
     </div>
-    <div class="col-sm-3 align-middle">
-      <img src="http://www.freeiconspng.com/uploads/book-icon--icon-search-engine-6.png"/>
+    <h4>Super Fast</h4>
+    <span>Single language in the entire applicaton. No need to learn SQL or an ORM API.</span>
+  </div>
+  <div class="col-sm-4">
+    <div class="text-center">
+        <img src="http://icons.iconarchive.com/icons/icons8/android/512/Measurement-Units-Speed-icon.png" class="rounded" alt="Speed">
     </div>
-</a>
+    <h4>Run Anywhere</h4>
+    <span>Single language in the entire applicaton. No need to learn SQL or an ORM API.</span>
   </div>
-  </div>
-</div> <!-- /container -->
+</div>
 <?php get_footer(); ?>
