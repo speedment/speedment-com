cdcdb80b45794baff7b31c7460bd4d15b78bedc8
pyknic <emil@speedment.com>
2017-03-29 15:29:55 -0700

    Update frontpage button

--- index.php
+++ index.php
@@ -32,23 +32,25 @@
             <label class="sr-only" for="inlineFormInput">Name</label>
             <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0"
             id="inlineFormInput" placeholder="Enter your work email">
-            <button type="submit" class="btn btn-success">Try For Free</button>
+            <button type="submit" class="btn">Try For Free</button>
           </form>
        </div>
       </div>
   </div>
 </div>
 
-<div class="row">
-  <?php if (dynamic_sidebar('home_description')): else: endif; ?>
+<div class="row" id="speedment-description">
+  <div class="col-6">
+    <?php if (dynamic_sidebar('home_description')): else: endif; ?>
+  </div>
 </div>
 <div class="row justify-content-md-center" id="why-speedment">
   <?php if (dynamic_sidebar('big_boxes')): else: endif; ?>
 </div>
-<div class="row justify-content-md-center">
+<div class="row justify-content-md-center" id="small-boxes">
   <?php if (dynamic_sidebar('small_boxes')): else: endif; ?>
 </div>
-<div class="row justify-content-md-center">
+<div class="row justify-content-md-center" id="company-logos">
   <?php if (dynamic_sidebar('company_logos')): else: endif; ?>
 </div>
 <?php get_footer(); ?>
