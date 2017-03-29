4167854e878d3efd21f14d60d8555f025d638049
pyknic <emil@speedment.com>
2017-03-29 15:03:12 -0700

    Change id of white navbar

--- front-page.php
+++ front-page.php
@@ -1,33 +1,5 @@
 <?php get_header(); ?>
 
-<h3>Top left menu</h3>
-<nav class="navbar navbar-toggleable-md navbar-light bg-faded" role="navigation">
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <?php wp_nav_menu(array(
-      'menu'              => 'top-left-menu',
-      'theme_location'    => 'top-left-menu',
-      'depth'             => 2,
-      'container'         => 'div',
-      'container_class'   => 'collapse navbar-collapse',
-      'container_id'      => 'navbarNavAltMarkup',
-      'menu_class'        => 'navbar-nav',
-      'walker'            => new bootstrap_4_walker_nav_menu())
-    ); ?>
-    <?php wp_nav_menu(array(
-      'menu'              => 'top-left-menu',
-      'theme_location'    => 'top-left-menu',
-      'depth'             => 2,
-      'container'         => 'div',
-      'container_class'   => 'collapse navbar-collapse',
-      'container_id'      => 'navbarNavAltMarkup',
-      'menu_class'        => 'navbar-nav',
-      'walker'            => new bootstrap_4_walker_nav_menu())
-    ); ?>
-</nav>
-
 <!-- Main hero unit for a primary marketing message or call to action -->
 <div class="jumbotron jumbotron-fluid" id="first-view" style="
   background-image: url(<?php header_image(); ?>);">
--- header.php
+++ header.php
@@ -17,7 +17,7 @@
   <?php wp_head(); ?>
 </head>
 <body>
-  <nav class="navbar navbar-toggleable-md navbar-light bg-faded" role="navigation">
+  <nav class="navbar-toggleable-md navbar-light bg-faded" role="navigation">
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#developer-navbar" aria-controls="developer-navbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -34,7 +34,7 @@
       ); ?>
   </nav>
   <nav class="navbar navbar-toggleable-md navbar-light bg-faded" role="navigation">
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#speedment-navbar" aria-controls="speedment-navbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <!-- Brand and toggle get grouped for better mobile display -->
@@ -44,7 +44,7 @@
         'depth'             => 2,
         'container'         => 'div',
         'container_class'   => 'collapse navbar-collapse',
-        'container_id'      => 'navbarNavAltMarkup',
+        'container_id'      => 'speedment-navbar',
         'menu_class'        => 'navbar-nav',
         'walker'            => new bootstrap_4_walker_nav_menu())
       ); ?>
