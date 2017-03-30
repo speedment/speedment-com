ee449ecce5190bf541ad6d117cd59b8ab20ec249
Unknown <john.rossberg@gmail.com>
2017-03-30 14:51:55 -0700

    #5 fixed bug

--- assets/js/animate-logos.js
+++ assets/js/animate-logos.js
@@ -3,17 +3,20 @@
     var showCompany = 4;
     var $list = $('#company-logos').find('div[data-info="company-logos"]');
     var $wrapper = $('div[data-tag="company-logos-wrapper"]');
+    updateIcons();
     window.setInterval(function () {
-      var $newArray = shuffle($list);
+      updateIcons();
+    }, 3000);
+    function updateIcons() {
+        shuffle($list);
         $wrapper.fadeOut("slow", function () {
           $wrapper.html('');
           $wrapper.show();
-          $wrapper.append($newArray.slice(0, showCompany));
-          $newArray.slice(0, showCompany).hide();
+          $wrapper.append($list.slice(0, showCompany));
+          $list.slice(0, showCompany).hide();
           $wrapper.find('div[data-info="company-logos"]').fadeIn('slow');
         });
-      $list = $newArray;
-    }, 3000);
+    }
     function shuffle(oldArray) {
       var array = oldArray.slice();
       var currentIndex = array.length,
