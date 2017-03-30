aee2526f50209853c2aa7071a55552283156e43c
pyknic <emil@speedment.com>
2017-03-29 17:59:33 -0700

    Remove unnescessary <div>-tags from footer

--- footer.php
+++ footer.php
@@ -1,17 +1,14 @@
-    <footer>
-      <div class="container-fluid" id="footer">
-      <div class="row">
-        <nav class="navbar  navbar-light bg-faded" role="navigation">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <?php wp_nav_menu( array(
-              'menu'           => 'footer-menu',
-              'theme_location' => 'footer-menu',
-              'container'      => false,
-              'depth'          => 2,
-              'walker'         => new Footer_Menu_Walker())
-            ); ?>
-        </nav>
-      </div>
+    <footer class="container-fluid" id="footer">
+      <nav class="row" role="navigation">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <?php wp_nav_menu( array(
+            'menu'           => 'footer-menu',
+            'theme_location' => 'footer-menu',
+            'container'      => false,
+            'depth'          => 2,
+            'walker'         => new Footer_Menu_Walker())
+          ); ?>
+      </nav>
       <div id="sharing-icons" class="d-flex justify-content-end">
         <i class="fa fa-facebook-square" aria-hidden="true"></i>
         <i class="fa fa-twitter" aria-hidden="true"></i>
