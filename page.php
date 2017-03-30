6ec7c13502e3bac41894722a506d119b8ece73e2
pyknic <emil@speedment.com>
2017-03-30 11:20:29 -0700

    Update page.php

--- page.php
+++ page.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
 <!-- Start Posts Section -->
-<?php the_content() ?>
+<?php while ( have_posts() ) : the_post();
+  the_content();
+endwhile; ?>
 <!-- End Posts Section -->
 <?php get_footer(); ?>
