faee6388d929e117c22a508f3145f5f0f8382dfa
pyknic <emil@speedment.com>
2017-03-29 17:55:30 -0700

    Remove wrapping for items in footer walker

--- wp-footer-walker.php
+++ wp-footer-walker.php
@@ -25,11 +25,11 @@
       $attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr($item->xfn) . '"' : '';
       $attributes .= ! empty( $item->url ) ? ' href="' . esc_attr($item->url) . '"' : '';
 
-      $item_output = $args->before;
+      //$item_output = $args->before;
       $item_output .= '<a' . $attributes . '>';
       $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
       $item_output .= '</a>';
-      $item_output .= $args->after;
+      //$item_output .= $args->after;
 
       $output .= ($depth > 0) ? '<li>' : '';
       $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
