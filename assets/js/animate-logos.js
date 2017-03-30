9ad2337ee9f5a73455241970b43c30bdb400f08b
Unknown <john.rossberg@gmail.com>
2017-03-30 15:03:11 -0700

    #5

--- assets/js/animate-logos.js
+++ assets/js/animate-logos.js
@@ -2,23 +2,23 @@
   $( document ).ready(function() {
     var showCompany = 4;
     var $list = $('#company-logos').find('div[data-info="company-logos"]');
-    var $wrapper = $('div[data-tag="company-logos-wrapper"]');
-    updateIcons();
+    var $wrapper = $('div[data-info="company-logos-wrapper"]');
+    showIcons();
     window.setInterval(function () {
-      updateIcons();
-    }, 3000);
-    function updateIcons() {
-        shuffle($list);
+      shuffle($list);
         $wrapper.fadeOut("slow", function () {
-          $wrapper.html('');
-          $wrapper.show();
-          $wrapper.append($list.slice(0, showCompany));
-          $list.slice(0, showCompany).hide();
-          $wrapper.find('div[data-info="company-logos"]').fadeIn('slow');
+          showIcons();
         });
+    }, 3000);
+    function showIcons() {
+      $wrapper.html('');
+      $wrapper.show();
+      $wrapper.append($list.slice(0, showCompany));
+      $list.slice(0, showCompany).hide();
+      $wrapper.find('div[data-info="company-logos"]').fadeIn('slow');
     }
     function shuffle(oldArray) {
-      var array = oldArray.slice();
+      var array = oldArray;
       var currentIndex = array.length,
         temporaryValue,
         randomIndex;
