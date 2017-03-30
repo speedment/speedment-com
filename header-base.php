b8ea74ee62bebd9acb4e3ae870a67239ae383b63
Unknown <john.rossberg@gmail.com>
2017-03-30 15:41:52 -0700

    Changed css

--- header-base.php
+++ header-base.php
@@ -17,7 +17,7 @@
   <?php wp_head(); ?>
 </head>
 <body>
-  <nav class="navbar-toggleable-md navbar-light bg-faded hidden-md-down" role="navigation">
+  <nav class="navbar-toggleable-md navbar-light bg-faded hidden-sm-down" role="navigation">
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#developer-navbar" aria-controls="developer-navbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -33,7 +33,7 @@
         'walker'            => new bootstrap_4_walker_nav_menu())
       ); ?>
   </nav>
-  <nav class="navbar navbar-toggleable-md navbar-light bg-faded hidden-md-down" role="navigation">
+  <nav class="navbar navbar-toggleable-md navbar-light bg-faded hidden-sm-down" role="navigation">
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#speedment-navbar" aria-controls="speedment-navbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
