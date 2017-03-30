0bc1db2e38279e932c396cfe90705e46df4a0588
Unknown <john.rossberg@gmail.com>
2017-03-30 11:25:42 -0700

    added document-ready

--- assets/js/animate-logos.js
+++ assets/js/animate-logos.js
@@ -1,25 +1,27 @@
 (function($) {
-      var $listHide = $('#company-logos').find('div[data-info="comapny-logos"]');
-      var $listShow = [];
-      var numElements = Math.min($listHide.length-1, 4);
-      for(var i = 0; i < numElements; i += 1){
-        $listShow.push(getRandomElement($listHide));
-      }
-      $listShow.forEach(function(e){
-        $(e).fadeIn("slow");
-      });
-      window.setInterval(function(){
-          var tempHide = getRandomElement($listShow);
-          var tempShow = getRandomElement($listHide);
-          $(tempHide).fadeOut("slow", function(){
-            $(tempShow).fadeIn("slow");
-          });
-          $listShow.push(tempShow);
-          $listHide.push(tempHide);
-          console.log($listShow, $listHide);
-      }, 3000);
-  function getRandomElement(list){
-    var index = Math.floor(Math.random() * list.length);
-    return list.splice(index,1)[0];
-  }
+  $( document ).ready(function() {
+    var $listHide = $('#company-logos').find('div[data-info="comapny-logos"]');
+    var $listShow = [];
+    var numElements = Math.min($listHide.length-1, 4);
+    for(var i = 0; i < numElements; i += 1){
+      $listShow.push(getRandomElement($listHide));
+    }
+    $listShow.forEach(function(e){
+      $(e).fadeIn("slow");
+    });
+    window.setInterval(function(){
+        var tempHide = getRandomElement($listShow);
+        var tempShow = getRandomElement($listHide);
+        $(tempHide).fadeOut("slow", function(){
+          $(tempShow).fadeIn("slow");
+        });
+        $listShow.push(tempShow);
+        $listHide.push(tempHide);
+        console.log($listShow, $listHide);
+    }, 3000);
+function getRandomElement(list){
+  var index = Math.floor(Math.random() * list.length);
+  return list.splice(index,1)[0];
+}
+});
 })( jQuery );
