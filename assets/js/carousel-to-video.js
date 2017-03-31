9de6a1fae2328bed32eb7787ffe43ec40e7cedae
Unknown <john.rossberg@gmail.com>
2017-03-31 11:26:06 -0700

    URl

--- assets/js/carousel-to-video.js
+++ assets/js/carousel-to-video.js
@@ -4,12 +4,12 @@
     console.log('ready to go!!');
       $('#front-page-video-modal').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget) // Button that triggered the modal
-        var recipient = button.data('video') // Extract info from data-* attributes
+        var url = button.data('video') // Extract info from data-* attributes
         // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
         // Update the modal's content. We'll use jQuery here, but you could use a data binding library or
         // other methods instead.
         var modal = $(this)
-        modal.find('.modal-title').text('New message to ' + recipient)
+        modal.find('iframe').attr('src', url);
       })
   });
 })(jQuery);
