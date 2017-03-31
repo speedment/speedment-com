af1b9da6c35a7c5521dc7c7deda4b6d13c8906e0
Unknown <john.rossberg@gmail.com>
2017-03-31 13:52:44 -0700

    Make modal larger

--- front-page.php
+++ front-page.php
@@ -106,8 +106,8 @@
     </div>
   </div>
 
-<div class="modal fade" id="front-page-video-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+<div class="modal fade " id="front-page-video-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
         <div class="modal-body">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
