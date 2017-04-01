efc363674a3df849cb8c8eb977a69ae791f31586
pyknic <emil@speedment.com>
2017-03-31 17:01:58 -0700

    Make two-column img fill entire column

--- two-columns-page.php
+++ two-columns-page.php
@@ -5,15 +5,15 @@
  */
 get_header(); ?>
 
-<div class="container-fluid">
+<div class="container-fluid" id="two-column-layout">
   <div class="row justify-content-center">
     <div class="container">
       <?php while (have_posts()) : the_post(); ?>
       <div class="row justify-content-center">
-        <div class="col-md-8 col-sm-12">
+        <div class="col-md-8 col-sm-12 left-side">
           <?php the_content(); ?>
         </div>
-        <div class="col-md-4 col-sm-12">
+        <div class="col-md-4 col-sm-12 right-side">
           <?php the_post_thumbnail('medium_large'); ?>
         </div>
       </div>
