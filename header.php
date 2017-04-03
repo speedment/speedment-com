9f84d7518c830190fcf357050f8fa6f51724f9a2
pyknic <emil@speedment.com>
2017-04-03 14:04:30 -0700

    Put header inside header tag

--- header.php
+++ header.php
@@ -1,21 +1,22 @@
 <?php get_header('base'); ?>
 
-<!--
-    Main Hero (#1 Tool to Accelerate Existing Database Applications)
--->
-<div class="jumbotron jumbotron-fluid" id="subpage-hero">
-    <div class="row justify-content-md-center">
-      <div class="col">
-        <div class="container">
-          <div class="row justify-content-md-left">
-            <div class="col-md-7 col-md-auto">
-              <div class="col-md-auto" id="first-view-text-wrapper">
-                <h2><?php single_post_title(); ?></h2>
-                <div class="breadcrumb"><?php dimox_breadcrumbs(); ?></div>
-             </div>
+  <!--
+      Main Hero (#1 Tool to Accelerate Existing Database Applications)
+  -->
+  <div class="jumbotron jumbotron-fluid" id="subpage-hero">
+      <div class="row justify-content-md-center">
+        <div class="col">
+          <div class="container">
+            <div class="row justify-content-md-left">
+              <div class="col-md-7 col-md-auto">
+                <div class="col-md-auto" id="first-view-text-wrapper">
+                  <h2><?php single_post_title(); ?></h2>
+                  <div class="breadcrumb"><?php dimox_breadcrumbs(); ?></div>
+               </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+    </div>
   </div>
-</div>
+</header>
