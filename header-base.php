bdc67a520fb6b021abb9d6bd1b13014a03d1f78d
pyknic <emil@speedment.com>
2017-04-03 14:03:10 -0700

    Put header in container

--- header-base.php
+++ header-base.php
@@ -17,65 +17,67 @@
   <?php wp_head(); ?>
 </head>
 <body>
-  <div id="nav-bar-wrapper"<?php echo is_user_logged_in() ? ' class="logged-in"' : ''; ?>>
-    <nav class="navbar-toggleable-md navbar-light bg-faded hidden-md-down" role="navigation">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#developer-navbar" aria-controls="developer-navbar" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <?php wp_nav_menu(array(
-          'menu'              => 'top-right-menu',
-          'theme_location'    => 'top-right-menu',
-          'depth'             => 2,
-          'container'         => 'div',
-          'container_class'   => 'collapse navbar-collapse justify-content-end',
-          'container_id'      => 'developer-navbar',
-          'menu_class'        => 'navbar-nav',
-          'walker'            => new bootstrap_4_walker_nav_menu())
-        ); ?>
-    </nav>
-    <nav class="navbar navbar-toggleable-md navbar-light bg-faded hidden-md-down" role="navigation">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#speedment-navbar" aria-controls="speedment-navbar" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+  <header class="container-fluid">
+    <div id="nav-bar-wrapper"<?php echo is_user_logged_in() ? ' class="logged-in"' : ''; ?>>
+      <nav class="navbar-toggleable-md navbar-light bg-faded hidden-md-down" role="navigation">
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#developer-navbar" aria-controls="developer-navbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <?php wp_nav_menu(array(
+            'menu'              => 'top-right-menu',
+            'theme_location'    => 'top-right-menu',
+            'depth'             => 2,
+            'container'         => 'div',
+            'container_class'   => 'collapse navbar-collapse justify-content-end',
+            'container_id'      => 'developer-navbar',
+            'menu_class'        => 'navbar-nav',
+            'walker'            => new bootstrap_4_walker_nav_menu())
+          ); ?>
+      </nav>
+      <nav class="navbar navbar-toggleable-md navbar-light bg-faded hidden-md-down" role="navigation">
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#speedment-navbar" aria-controls="speedment-navbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-        <a class="navbar-brand" href="/">
-          <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/brand/cross.svg" width="30" height="30" class="d-inline-block align-top" alt="Speedment">
-          Speedment
-        </a>
+          <a class="navbar-brand" href="/">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/brand/cross.svg" width="30" height="30" class="d-inline-block align-top" alt="Speedment">
+            Speedment
+          </a>
 
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <?php wp_nav_menu(array(
-          'menu'              => 'top-left-menu',
-          'theme_location'    => 'top-left-menu',
-          'depth'             => 2,
-          'container'         => 'div',
-          'container_class'   => 'collapse navbar-collapse',
-          'container_id'      => 'speedment-navbar',
-          'menu_class'        => 'navbar-nav',
-          'walker'            => new bootstrap_4_walker_nav_menu())
-        ); ?>
-    </nav>
-    <nav class="navbar navbar-toggleable-md navbar-light bg-faded hidden-lg-up" role="navigation">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#mobile-navbar" aria-controls="mobile-navbar" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <?php wp_nav_menu(array(
+            'menu'              => 'top-left-menu',
+            'theme_location'    => 'top-left-menu',
+            'depth'             => 2,
+            'container'         => 'div',
+            'container_class'   => 'collapse navbar-collapse',
+            'container_id'      => 'speedment-navbar',
+            'menu_class'        => 'navbar-nav',
+            'walker'            => new bootstrap_4_walker_nav_menu())
+          ); ?>
+      </nav>
+      <nav class="navbar navbar-toggleable-md navbar-light bg-faded hidden-lg-up" role="navigation">
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#mobile-navbar" aria-controls="mobile-navbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-        <a class="navbar-brand" href="/">
-          <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/brand/cross.svg" width="30" height="30" class="d-inline-block align-top" alt="Speedment">
-          Speedment Mobile
-        </a>
+          <a class="navbar-brand" href="/">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/brand/cross.svg" width="30" height="30" class="d-inline-block align-top" alt="Speedment">
+            Speedment Mobile
+          </a>
 
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <?php wp_nav_menu(array(
-          'menu'              => 'top-mobile-menu',
-          'theme_location'    => 'top-mobile-menu',
-          'depth'             => 2,
-          'container'         => 'div',
-          'container_class'   => 'collapse navbar-collapse',
-          'container_id'      => 'mobile-navbar',
-          'menu_class'        => 'navbar-nav',
-          'walker'            => new bootstrap_4_walker_nav_menu())
-        ); ?>
-    </nav>
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <?php wp_nav_menu(array(
+            'menu'              => 'top-mobile-menu',
+            'theme_location'    => 'top-mobile-menu',
+            'depth'             => 2,
+            'container'         => 'div',
+            'container_class'   => 'collapse navbar-collapse',
+            'container_id'      => 'mobile-navbar',
+            'menu_class'        => 'navbar-nav',
+            'walker'            => new bootstrap_4_walker_nav_menu())
+          ); ?>
+      </nav>
+    </div>
 </div>
