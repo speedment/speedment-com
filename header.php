1bcde151c2d61a4aa6ede3dd14be455313188ae9
pyknic <emil@speedment.com>
2017-04-03 14:13:33 -0700

    Move subpage header to align with content

--- header.php
+++ header.php
@@ -3,20 +3,18 @@
   <!--
       Main Hero (#1 Tool to Accelerate Existing Database Applications)
   -->
-  <div class="row jumbotron jumbotron-fluid" id="subpage-hero">
-      <div class="row justify-content-md-center">
-        <div class="col">
-          <div class="container">
-            <div class="row justify-content-md-left">
-              <div class="col-md-7 col-md-auto">
-                <div class="col-md-auto" id="first-view-text-wrapper">
-                  <h2><?php single_post_title(); ?></h2>
-                  <div class="breadcrumb"><?php dimox_breadcrumbs(); ?></div>
-               </div>
-              </div>
-            </div>
+  <div class="row jumbotron jumbotron-fluid justify-content-md-center" id="subpage-hero">
+    <div class="col">
+      <div class="container">
+        <div class="row justify-content-md-left">
+          <div class="col-md-7 col-md-auto">
+            <div class="col-md-auto" id="first-view-text-wrapper">
+              <h2><?php single_post_title(); ?></h2>
+              <div class="breadcrumb"><?php dimox_breadcrumbs(); ?></div>
+           </div>
           </div>
         </div>
+      </div>
     </div>
   </div>
 </header>
