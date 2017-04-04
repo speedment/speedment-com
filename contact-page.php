dfb59482e96dd64fc19883001b5f2de63b4241dc
pyknic <emil@speedment.com>
2017-04-04 16:20:09 -0700

    Add form logic to contact pages

--- contact-page.php
+++ contact-page.php
@@ -3,10 +3,95 @@
  * Template Name: Contact Page
  * Description: Page template with a Contact Us form below the text
  */
+
+$my_email   = "";
+$my_name    = "";
+$my_text    = "";
+$my_error   = false;
+$my_success = false;
+
+if (isset($_POST['myEmail'])) $my_email = trim($_POST['myEmail']);
+if (isset($_POST['myName']))  $my_name  = trim($_POST['myName']);
+if (isset($_POST['myText']))  $my_text  = trim($_POST['myText']);
+
+if ($my_email != "" && $my_name != "" && $my_text != "") {
+  $subject = 'New Message From ' . esc_html($my_name);
+  if (!wp_mail('info@speedment.com', $subject,
+    '<!DOCTYPE html><html><head><title>' . $subject . '</title></head>' .
+    '<body>' .
+      '<p><b>From:</b> ' . esc_html($my_name) . '</p>' .
+      '<p><b>Email:</b> ' . esc_html($my_email) . '</p>' .
+      '<p><b>Message:</b> ' . esc_html($my_message) . '</p>' .
+      '<hr />' .
+      '<small>This email was generated automatically using a form on the "www.speedment.com"-website.</small>'
+    '</body></html>',
+    array(
+      'Content-Type: text/html; charset=UTF-8',
+      'From: speedment.com <noreply@speedment.com>',
+      "Reply-To: $my_name <$my_email>"
+    )
+  )) {
+    $my_error = "Error! Make sure specified address '$my_email' is correct.";
+  } else {
+    $my_success = true;
+  }
+}
+
 get_header(); ?>
 <!-- Start Page Content -->
-<?php while (have_posts()) : the_post();
-  the_content();
-endwhile; ?>
+
+<div class="row justify-content-center" id="contact">
+  <div class="container">
+    <!--
+        Contact Page Content
+    -->
+    <div class="row justify-content-center product-page">
+      <div class="col">
+        <!-- Start Page Content -->
+        <?php while (have_posts()) : the_post();
+          the_content();
+        endwhile; ?>
+      </div>
+    </div>
+
+    <!--
+        Contact Widget Area
+    -->
+    <?php if ($my_success): ?>
+      <div class="row justify-content-center">
+        <div class="col">
+          <div class="alert alert-success" role="alert">
+            <strong>Thank you!</strong> Your message has been sent. We will get back to you as soon as we can.
+          </div>
+        </div>
+      </div>
+    <?php else: ?>
+      <form class="row justify-content-left" action="?" method="POST">
+        <div class="form-group">
+          <label for="myName">Name</label>
+          <input name="myName" type="text" class="form-control" id="myName" placeholder="Enter name" value="<?php echo $my_name; ?>">
+        </div>
+
+        <div class="form-group<?php if ($my_error) echo ' has-danger'; ?>">
+          <label for="myEmail">Email</label>
+          <input name="myEmail" type="email" class="form-control<?php if ($my_error) echo ' form-control-danger'; ?>" id="myEmail" placeholder="Enter email" value="<?php echo $my_email; ?>">
+          <?php if ($my_error): ?><div class="form-control-feedback"><?php echo $my_error; ?></div><?php endif; ?>
+        </div>
+
+        <div class="form-group">
+          <label for="myText">Message</label>
+          <textarea name="myText" class="form-control" id="myText" placeholder="Enter message here"><?php echo $my_text; ?></textarea>
+        </div>
+
+        <div class="form-group">
+          <div class="g-recaptcha" data-sitekey="6LdamhsUAAAAAJmGlLANew3Q4Kw-lZ3e54iTfwBa"></div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    <?php endif; ?>
+  </div>
+</div>
+
 <!-- Start Page Content -->
 <?php get_footer(); ?>
