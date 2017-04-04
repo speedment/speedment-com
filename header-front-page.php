5a1b181befc316c75f6bc3ac030c3caa738c6895
pyknic <emil@speedment.com>
2017-04-04 09:42:10 -0700

    Fix header font size depending on screen size

--- header-front-page.php
+++ header-front-page.php
@@ -6,7 +6,7 @@
   <div class="row jumbotron jumbotron-fluid" id="first-view" style="background-image: url('<?php header_image(); ?>');">
     <div class="col">
       <div class="container">
-        <div class="row justify-content-md-center">
+        <div class="row justify-content-center">
           <div class="col-lg-7 pull-lg-2
                       col-md-8 pull-md-1
                       col-sm-9 pull-sm-0" id="first-view-text-wrapper">
