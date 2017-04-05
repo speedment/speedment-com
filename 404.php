1bae33e11fe92f32761264049c2c4299dbfc717d
pyknic <emil@speedment.com>
2017-04-05 16:01:00 -0700

    Create 404 template

---
+++ 404.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Template Name: 404 Page
+ * Description: Page template that is used when a resource can't be found.
+ */
+get_header(); ?>
+<!-- Start Page Content -->
+
+<div class="justify-content-center" id="notFound">
+  <div class="container">
+    <!--
+        Gallery Page Content
+    -->
+    <div class="row justify-content-center gallery-page">
+      <div class="col">
+        <p>Could not find the requested page.</p>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<!-- Start Page Content -->
+<?php get_footer(); ?>
