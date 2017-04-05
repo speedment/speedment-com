d77b79880d2206ea113241b9b2a63af02a550d7c
pyknic <emil@speedment.com>
2017-04-05 16:07:38 -0700

    Add custom header to 404 pages

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
         <div class="row justify-content-md-left">
           <div class="col-md-7 col-md-auto">
             <div class="col-md-auto" id="first-view-text-wrapper">
-              <h2><?php single_post_title(); ?></h2>
+              <h2><?php if (is_404()) {echo '404 Page Not found';} else {single_post_title();} ?></h2>
               <div class="breadcrumb"><?php dimox_breadcrumbs(); ?></div>
            </div>
           </div>
