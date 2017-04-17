9af2fe890f3bb4a143323bc787ad8419b1f28ac6
pyknic <emil@speedment.com>
2017-04-17 15:42:15 -0700

    Create trial template

---
+++ trial-page.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Template Name: Trial Page
+ * Description: Page template that shows a trial license configurator form
+ */
+get_header(); ?>
+<!-- Start Page Content -->
+
+
+
+<!-- End Page Content -->
+<?php get_footer(); ?>
