8e267f814cd25692699be9fbc1990d43f77002d9
pyknic <emil@speedment.com>
2017-04-18 16:46:01 -0700

    Add select-all button to code fields

--- trial-page.php
+++ trial-page.php
@@ -141,6 +141,28 @@ get_header(); ?>
       <script>
         jQuery(document).ready(function($) {
 
+          Prism.plugins.toolbar.registerButton('select-code', function() {
+          	var button = document.createElement('button');
+          	button.innerHTML = 'Select Code';
+
+          	button.addEventListener('click', function () {
+          		// Source: http://stackoverflow.com/a/11128179/2757940
+          		if (document.body.createTextRange) { // ms
+          			var range = document.body.createTextRange();
+          			range.moveToElementText(env.element);
+          			range.select();
+          		} else if (window.getSelection) { // moz, opera, webkit
+          			var selection = window.getSelection();
+          			var range = document.createRange();
+          			range.selectNodeContents(env.element);
+          			selection.removeAllRanges();
+          			selection.addRange(range);
+          		}
+          	});
+
+          	return button;
+          });
+
           var groupId       = $('#inputGroupId');
           var artifactId    = $('#inputArtifactId');
           var version       = $('#inputVersion');
