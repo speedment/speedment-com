88ae774805431d50866640649a15b73ed3b316b1
pyknic <emil@speedment.com>
2017-04-18 13:08:27 -0700

    Change syntax highlighting for xml files

--- trial-page.php
+++ trial-page.php
@@ -249,7 +249,7 @@ get_header(); ?>
             }
           }
 
-          $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+          $('a[data-toggle="tab"]').on('shows.bs.tab', function (e) {
             updateCode();
           });
 
