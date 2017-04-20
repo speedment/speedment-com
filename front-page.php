e68792dc11c0275b1512443d0991376909e2e1b7
John Rossberg <rossii123@users.noreply.github.com>
2017-04-20 13:46:42 -0700

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -78,7 +78,7 @@
     VIDEO
   -->
    <div class="row justify-content-center" id="company-logos">
-     <div class="col">
+     <div class="col col-md-8">
          <div class="embed-responsive embed-responsive-16by9">
         <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/9OhoW4xy-kA" frameborder="0" allowfullscreen></iframe>
      </div>
