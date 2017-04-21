1fb9f82a2fdf2675ab0df8f5c94af9311773c9db
pyknic <emil@speedment.com>
2017-04-21 14:34:45 -0700

    Add container to features page

--- features-page.php
+++ features-page.php
@@ -5,8 +5,23 @@
  */
 get_header(); ?>
 <!-- Start Page Content -->
-<?php while (have_posts()) : the_post();
-  the_content();
-endwhile; ?>
+
+<div class="justify-content-center" id="gallery">
+  <div class="container">
+    <!--
+        Features Page Content
+    -->
+    <div class="row justify-content-center features-page">
+      <div class="col">
+        <?php while (have_posts()) : the_post();
+          the_content();
+        endwhile; ?>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
 <!-- Start Page Content -->
 <?php get_footer(); ?>
