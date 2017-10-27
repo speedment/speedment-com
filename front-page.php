f2ccb38cfc2ada951ff7caf7756826e5b4325c9f
pyknic <emil@speedment.com>
2017-10-27 17:50:01 +0200

    Change YouTube video

--- front-page.php
+++ front-page.php
@@ -70,7 +70,7 @@
        <div class="row">
          <div class="col-12">
            <div class="embed-responsive embed-responsive-16by9">
-            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/9OhoW4xy-kA" frameborder="0" allowfullscreen></iframe>
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/89lVMmIr8MQ" frameborder="0" allowfullscreen></iframe>
           </div>
          </div>  
         </div>
