8f7ed00d2bcedf581b57371c69b05744372c10a6
Per-Åke Minborg <minborg@speedment.com>
2017-11-09 09:21:10 -0800

    Bump Speedment version to 3.0.17

--- trial-page.php
+++ trial-page.php
@@ -257,7 +257,7 @@ get_header(); ?>
               args['pluginArtifactId']  = 'speedment-enterprise-maven-plugin';
               args['pluginVersion']     = '${speedment.enterprise.version}';
             } else {
-              args['speedmentVersion']  = '3.0.16';
+              args['speedmentVersion']  = '3.0.17';
               args['runtimeGroupId']    = 'com.speedment';
               args['runtimeArtifactId'] = 'runtime';
               args['runtimeVersion']    = '${speedment.version}';
