87b81786ba87f3283be989d362e1069062169b20
Per-Åke Minborg <minborg@speedment.com>
2017-12-11 13:44:38 -0800

    Bump version to 1.1.16 Enterprise

--- trial-page.php
+++ trial-page.php
@@ -249,7 +249,7 @@ get_header(); ?>
             };
 
             if (enterprise) {
-              args['speedmentEnterpriseVersion'] = '1.1.15';
+              args['speedmentEnterpriseVersion'] = '1.1.16';
               args['runtimeGroupId']    = 'com.speedment.enterprise';
               args['runtimeArtifactId'] = 'runtime';
               args['runtimeVersion']    = '${speedment.enterprise.version}';
