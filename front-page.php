ebc5247fee3bda232f2a8fb1170c81bffe4aed56
julgus <gustafsson.julia@live.se>
2018-02-13 13:04:20 -0800

    Adjust video link on front page

--- front-page.php
+++ front-page.php
@@ -70,7 +70,7 @@
        <div class="row">
          <div class="col-12">
            <div class="embed-responsive embed-responsive-16by9">
-            <iframe class="embed-responsive-item" src="https://www.youtube.com/watch?v=-R7C18LZa8o" frameborder="0" allowfullscreen></iframe>
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/-R7C18LZa8o" frameborder="0" allowfullscreen></iframe>
           </div>
          </div>  
         </div>
