b0a31c0601312a963e60e725f4b985b2084bd68c
Per-Åke Minborg <minborg@speedment.com>
2018-05-16 20:58:02 -0700

    Bump version to 1.2.0 Emerson

--- trial-page.php
+++ trial-page.php
@@ -274,7 +274,7 @@ get_header(); ?>
             }
 
             if (enterprise) {
-              args['speedmentEnterpriseVersion'] = '1.1.17';
+              args['speedmentEnterpriseVersion'] = '1.2.0';
               args['runtimeGroupId']    = 'com.speedment.enterprise';
               args['runtimeArtifactId'] = 'runtime';
               args['runtimeVersion']    = '${speedment.enterprise.version}';
