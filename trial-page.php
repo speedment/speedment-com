6104f80af308cdd90126cf742c12fde489f7019c
Per-Åke Minborg <minborg@speedment.com>
2018-06-06 19:55:21 -0700

    Update trial-page.php

--- trial-page.php
+++ trial-page.php
@@ -274,7 +274,7 @@ get_header(); ?>
             }
 
             if (enterprise) {
-              args['speedmentEnterpriseVersion'] = '1.2.0';
+              args['speedmentEnterpriseVersion'] = '3.1.2';
               args['runtimeGroupId']    = 'com.speedment.enterprise';
               args['runtimeArtifactId'] = 'runtime';
               args['runtimeVersion']    = '${speedment.enterprise.version}';
