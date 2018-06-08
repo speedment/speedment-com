6e7f65a4d5ca99f291957b9825084bef7b3f8b3f
Per-Åke Minborg <minborg@speedment.com>
2018-06-08 14:39:12 -0700

    Update trial-page.php

--- trial-page.php
+++ trial-page.php
@@ -282,7 +282,7 @@ get_header(); ?>
               args['pluginArtifactId']  = 'speedment-enterprise-maven-plugin';
               args['pluginVersion']     = '${speedment.enterprise.version}';
             } else {
-              args['speedmentVersion']  = '3.1.2';
+              args['speedmentVersion']  = '3.1.3';
               args['runtimeGroupId']    = 'com.speedment';
               args['runtimeArtifactId'] = 'runtime';
               args['runtimeVersion']    = '${speedment.version}';
