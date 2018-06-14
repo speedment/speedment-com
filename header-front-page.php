b460567a90886c825de2f20323ddc97d5080e0f0
julgus <gustafsson.julia@live.se>
2018-06-14 12:47:41 -0700

    Try add button elsewhere

--- header-front-page.php
+++ header-front-page.php
@@ -11,6 +11,7 @@
                       col-md-8 pull-md-1
                       col-sm-9 pull-sm-0" id="first-view-text-wrapper">
             <h1><?php echo get_bloginfo('description'); ?></h1>
+            <button type="submit" class="btn btn-success">Try For Free</button>
             <form class="lead" id="leadForm" action="#">
               <label class="sr-only" for="inlineFormInput">Name</label>
               <input type="email" name="email" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Enter your work email" required="required">
