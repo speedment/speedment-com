bbddfaa6efb17b46c6c53858b22ba7bd86ce4f48
julgus <gustafsson.julia@live.se>
2018-06-14 12:51:51 -0700

    Surround button with form class

--- header-front-page.php
+++ header-front-page.php
@@ -11,7 +11,10 @@
                       col-md-8 pull-md-1
                       col-sm-9 pull-sm-0" id="first-view-text-wrapper">
             <h1><?php echo get_bloginfo('description'); ?></h1>
-            <a href="http://speedment.com/initializer" class="btn btn-primary"Try For Free</a>
+            
+            <form class="lead" id="leadForm" action="#">
+              <a href="http://speedment.com/initializer" class="btn btn-primary"Try For Free</a>
+            </form>
           </div>
         </div>
       </div>
