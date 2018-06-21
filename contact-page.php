82fb9dc23a93e07d0cac9404a64ce80dc5fd309a
julgus <gustafsson.julia@live.se>
2018-06-20 20:44:33 -0700

    Add column with link to Gitter

--- contact-page.php
+++ contact-page.php
@@ -78,7 +78,12 @@ get_header(); ?>
         Contact Page Content
     -->
     <div class="row justify-content-center product-page">
-      <div class="col">
+      <div class="col-lg-4 px-4 mx-2">
+          <h3>Technical Questions</h3>
+           <p>If you have a technical question, one of our developers will be happy to assist you.
+           You can get in direct contact with them in the <a href="https://gitter.im/speedment/speedment" target="_blank">Gitter Chatroom</a>.</p>
+        </div>
+        <div class="col-lg">
         <!-- Start Page Content -->
         <?php while (have_posts()) : the_post();
           the_content();
