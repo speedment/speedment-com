bc1e59810d533d692761015d2135219bca90d1ef
julgus <gustafsson.julia@live.se>
2018-08-15 17:27:44 +0200

    Remove breadcrumbs

--- header.php
+++ header.php
@@ -10,7 +10,6 @@
           <div class="col-md-7 col-md-auto">
             <div class="col-md-auto" id="first-view-text-wrapper">
               <h2><?php if (is_404()) {echo '404 Page Not found';} else {single_post_title();} ?></h2>
-              <div class="breadcrumb"><?php dimox_breadcrumbs(); ?></div>
            </div>
           </div>
         </div>
