c23d15dddb7a50a32006f015b346185185a573ed
Dan Lawesson <dan@kodgruvan.se>
2018-12-12 10:52:51 +0100

    Update default JDBC to latest mysql version

--- trial-page-OS.php
+++ trial-page-OS.php
@@ -53,7 +53,7 @@ get_header(); ?>
           <div class="form-group row">
             <label for="inputDriverVersion" class="col-sm-4 col-form-label">JDBC Driver Version</label>
             <div class="col-sm-8">
-              <input type="text" class="form-control" id="inputDriverVersion" placeholder="Database Driver Version" value="5.1.46">
+              <input type="text" class="form-control" id="inputDriverVersion" placeholder="Database Driver Version" value="8.0.13">
               <small id="helpDriverVersion" class="text-muted nosize" style="display:none">
                 Make sure this driver is available on your development machine.
               </small>
