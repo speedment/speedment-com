8a2c54a12b4612b6773597ed2291449685bea7ec
julgus <gustafsson.julia@live.se>
2019-03-26 14:45:02 +0100

    header-datastore: Make documentation btn secondary

--- header-datastore.php
+++ header-datastore.php
@@ -12,7 +12,7 @@
             <h3>Designed for commercial deployments where security, availability, and performance cannot be compromized.</h3>
               <div class="row">
                 <div class="col-6" id="product-view-button-area">
-                  <a href="https://speedment.github.io/speedment-doc/introduction.html" class="btn btn-primary">Documentation </a>
+                  <a href="https://speedment.github.io/speedment-doc/introduction.html" class="btn btn-secondary">Documentation </a>
                 </div>
                 <div class="col-6" id="product-view-button-area">
                   <a href="http://speedment.com/initializer" class="btn btn-primary">Download</a>
