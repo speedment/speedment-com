bb6de7df1061fe5fb1c6084535f9d5e1d166560a
julgus <gustafsson.julia@live.se>
2019-03-26 13:35:29 +0100

    header-datastore: Update image and buttons

--- header-datastore.php
+++ header-datastore.php
@@ -3,45 +3,23 @@
   <!--
       Main Hero (#1 Tool to Accelerate Existing Database Applications)
   -->
-  <div class="row jumbotron jumbotron-fluid" id="first-view" style="background-image: url('https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80');">
+  <div class="row jumbotron jumbotron-fluid" id="first-view" style="background-image: url('https://www.speedment.com/wp-content/uploads/2018/11/Speedment.jpeg');">
     <div class="col">
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-lg-7 px-5 pull-lg-2
                       col-md-8 pull-md-1
                       col-sm-9 pull-sm-0" id="first-view-text-wrapper">
-            <h1><?php echo get_bloginfo('description'); ?></h1>
+            <h1>DataStore</h1>
+            <h3>Designed for commercial deployments where security, availability, and performance cannot be compromized.</h3>
               <div class="row">
-                <div class="col" id="first-view-button-area">
-                  <a href="http://speedment.com/initializer" class="btn btn-primary">Try For Free</a>
+                <div class="col-6" id="first-view-button-area">
+                  <a href="https://speedment.github.io/speedment-doc/introduction.html" class="btn btn-primary">Documentation </a>
                 </div>
-              </div>
-            <form class="lead" id="leadForm" action="#">
-              <label class="sr-only" for="inlineFormInput">Name</label>
-                <input type="email" name="email" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Enter your work email" required="required">
-                  <button type="submit" class="btn btn-success">Try For Free</button>
-                  <script>
-                 jQuery(document).ready(function($) {
-                   $('#leadForm').submit(function(ev) {
-                     console.log("Submit form");
- 
-                     $.ajax({
-                       type: 'POST',
-                       url: 'https://api.speedment.com:9010/lead/curious/' + encodeURIComponent($('#leadForm input[name="email"]').val()),
-                       success: function() {
-                         $('#leadModal').modal('show');
-                       },
-                       error: function(msg) {
-                        console.err('Error sending lead to server.', msg); 
-                       }
-                     });
- 
-                     ev.preventDefault();
-                     return false;
-                   });
-                 });
-               </script>
-             </form>
+                <div class="col-6" id="first-view-button-area">
+                  <a href="http://speedment.com/initializer" class="btn btn-primary">Download</a>
+                </div>
+            </div>
           </div>
         </div>
       </div>
