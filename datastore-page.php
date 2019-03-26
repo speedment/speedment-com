eec1a2c1b5d4af6272c66459be849282c6bbcc33
julgus <gustafsson.julia@live.se>
2019-03-26 15:22:02 +0100

    datastore-page: Add database image

--- datastore-page.php
+++ datastore-page.php
@@ -9,12 +9,13 @@ get_header('datastore'); ?>
 
   <div class="row justify-content-center" id="speedment-description">
     <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-lg">
-          <?php if (dynamic_sidebar('home_description')): else: endif; ?>
+      <div class="row">
+        <div class="col" style="text-align: center">
+          <h1 style="text-align: center">Use any Database</h1>
+          <img src="https://www.speedment.com/wp-content/uploads/2018/05/SQL-Server-Logo.png" alt="" width="90%" class="aligncenter size-full wp-image-1470" />
         </div>
       </div>
-    </div>
+    </div> 
   </div>
 
   <div class="row justify-content-center" id="speedment-description">
