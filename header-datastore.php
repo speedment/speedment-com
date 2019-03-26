2f48a9d3db5d143e4513b9d9336b24158f327fac
julgus <gustafsson.julia@live.se>
2019-03-26 14:26:43 +0100

    header-datastore: Change btn-area id

--- header-datastore.php
+++ header-datastore.php
@@ -11,10 +11,10 @@
             <h1>DataStore</h1>
             <h3>Designed for commercial deployments where security, availability, and performance cannot be compromized.</h3>
               <div class="row">
-                <div class="col-6" id="first-view-button-area">
+                <div class="col-6" id="product-page-button-area">
                   <a href="https://speedment.github.io/speedment-doc/introduction.html" class="btn btn-primary">Documentation </a>
                 </div>
-                <div class="col-6" id="first-view-button-area">
+                <div class="col-6" id="product-page-button-area">
                   <a href="http://speedment.com/initializer" class="btn btn-primary">Download</a>
                 </div>
             </div>
