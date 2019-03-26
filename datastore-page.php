be92ae08e94c0d4d8cd11c539e2e4bbfeb902968
julgus <gustafsson.julia@live.se>
2019-03-26 15:06:13 +0100

    datastore-page: Try components

--- datastore-page.php
+++ datastore-page.php
@@ -7,5 +7,25 @@ get_header('datastore'); ?>
 
 <!-- Start Page Content -->
 
+  <div class="row justify-content-center" id="speedment-description">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg">
+          <?php if (dynamic_sidebar('home_description')): else: endif; ?>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row justify-content-center" id="speedment-description">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg">
+          <?php if (dynamic_sidebar('home_description')): else: endif; ?>
+        </div>
+      </div>
+    </div>
+  </div>
+
 <!-- End Page Content -->
 <?php get_footer(); ?>
