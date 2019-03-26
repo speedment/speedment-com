85995b3463bc5982f4270b2007e2cb9be61705ae
julgus <gustafsson.julia@live.se>
2019-03-26 13:14:55 +0100

    datastore-page: Name template

--- datastore-page.php
+++ datastore-page.php
@@ -1,1 +1,10 @@
-Base for DataStore page
+<?php
+/*
+ * Template Name: DataStore page
+ * Description: Page template that shows a DataStore page header
+ */
+get_header(); ?>
+<!-- Start Page Content -->
+
+<!-- End Page Content -->
+<?php get_footer(); ?>
