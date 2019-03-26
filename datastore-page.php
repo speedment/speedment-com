ee4e71dd77a09c6b021fa7be04fed9959a3e69f2
julgus <gustafsson.julia@live.se>
2019-03-26 13:15:55 +0100

    datastore-page: Try header from front-page

--- datastore-page.php
+++ datastore-page.php
@@ -3,7 +3,8 @@
  * Template Name: DataStore page
  * Description: Page template that shows a DataStore page header
  */
-get_header(); ?>
+get_header('front-page'); ?>
+
 <!-- Start Page Content -->
 
 <!-- End Page Content -->
