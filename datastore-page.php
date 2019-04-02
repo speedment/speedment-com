8a9ec46f7b21dcd1c582e65672ab943614a648fe
julgus <gustafsson.julia@live.se>
2019-04-02 16:43:45 +0200

    Add documentation and resource boxes

--- datastore-page.php
+++ datastore-page.php
@@ -30,25 +30,37 @@ get_header('datastore'); ?>
         </div>
       </div>
     </div> 
-  </div>
+</div>
 
-  <div class="row justify-content-center" id="datastore-resources">
-    <div class="container full-width-product-view">
-      <div class="row no-gutters justify-content-center">
-        <div class="col-lg-6" id="datastore-documentation">
-          <h1 style="text-align: center">Documentation</h1>
-        </div>
-        <div class="col-lg-6" id="datastore-download">
-          <h1 style="text-align: center">Download</h1>
-        </div>
-      </div>
-    </div>
-  </div>
-
-
-  <div class="row justify-content-center" id="datastore-resources">
+<div class="row justify-content-center" id="datastore-extras">
     <div class="container">
       <div class="row justify-content-center">
+        <div class="col-md mx-2 my-3 px-4" id="datastore-resources">
+          <div class="col title-area"> 
+           
+          </div>
+          <div class="col min-height">
+            <h3>Get Free Trial</h3>
+            <p>This whitepaper addresses the performance challenges for existing slow databases and presents a more modern solution – Speedment, a Stream ORM Java Toolkit and Runtime with extreme capabilities using an in JVM-memory data store. </p>
+          </div>
+          <div class="col center-button">
+            <a href="https://speedment.com/wp-content/uploads/2017/03/Speedment-White-Paper_2017.pdf" target="_blank" class="btn btn-secondary">Read Paper</a>
+          </div>
+        </div>
+        <div class="col-md mx-2 my-3 px-4" id="datastore-resources">
+          <div class="col title-area"> 
+
+          </div>
+          <div class="col min-height">
+            <h3>Documentation</h3>
+            <p>Building highly scalable and fast applications quickly sound like an oxymoron. 
+              And if we throw in the possibility of doing it for your current database and application, it sounds even more like a daydream. 
+              But combining two technology stacks that share the goal of great UX through awesome DX we can achieve the impossible.</p>
+          </div>
+          <div class="col center-button">
+            <a href="https://www.youtube.com/watch?v=Ive1fTTxUpU" target="_blank" class="btn btn-secondary">Watch Now</a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -56,41 +68,41 @@ get_header('datastore'); ?>
   <div class="row justify-content-center" id="datastore-extras">
     <div class="container">
       <div class="row justify-content-center">
-        <div class="col-md mx-2 my-3 px-0 gray">
-          <div class="col title-area px-3"> 
+        <div class="col-md mx-2 my-3 px-4 gray">
+          <div class="col title-area"> 
             Article
           </div>
-          <div class="col px-3 min-height">
+          <div class="col min-height">
             <h3>Database Actions Using Java 8 Stream Syntax Instead of SQL</h3>
             <p>Why should you need to use SQL when the same semantics can be derived directly from Java 8 streams? This article shows how the resemblance between streams and SQL commands can be used to gain ultra-fast access to data. </p>  
           </div>
-          <div class="col px-3 center-button">
+          <div class="col center-button">
             <a href="http://www.javamagazine.mozaicreader.com/MayJune2017/Default/34/0#&pageSet=34&page=34" target="_blank" class="btn btn-secondary">Read Article</a>
           </div>
         </div>
-        <div class="col-md mx-2 my-3 px-0 gray">
-          <div class="col title-area px-3"> 
+        <div class="col-md mx-2 my-3 px-4 gray">
+          <div class="col title-area"> 
             White Paper
           </div>
-          <div class="col px-3 min-height">
+          <div class="col min-height">
             <h3>Speedment the Java Stream ORM</h3>
             <p>This whitepaper addresses the performance challenges for existing slow databases and presents a more modern solution – Speedment, a Stream ORM Java Toolkit and Runtime with extreme capabilities using an in JVM-memory data store. </p>
           </div>
-          <div class="col px-3 center-button">
+          <div class="col center-button">
             <a href="https://speedment.com/wp-content/uploads/2017/03/Speedment-White-Paper_2017.pdf" target="_blank" class="btn btn-secondary">Read Paper</a>
           </div>
         </div>
-        <div class="col-md mx-2 my-3 px-0 gray d-md-none d-lg-block">
-          <div class="col title-area px-3"> 
+        <div class="col-md mx-2 my-3 px-4 gray d-md-none d-lg-block">
+          <div class="col title-area"> 
             Webinar
           </div>
-          <div class="col px-3 min-height">
+          <div class="col min-height">
             <h3>Building Super Fast Data-Driven Apps with Speedment and Vaadin</h3>
             <p>Building highly scalable and fast applications quickly sound like an oxymoron. 
               And if we throw in the possibility of doing it for your current database and application, it sounds even more like a daydream. 
               But combining two technology stacks that share the goal of great UX through awesome DX we can achieve the impossible.</p>
           </div>
-          <div class="col px-3 center-button">
+          <div class="col center-button">
             <a href="https://www.youtube.com/watch?v=Ive1fTTxUpU" target="_blank" class="btn btn-secondary">Watch Now</a>
           </div>
         </div>
