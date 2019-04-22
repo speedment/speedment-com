9033a05845974637a53ee53ad4ba7edaedab7d65
julgus <gustafsson.julia@live.se>
2019-04-22 16:24:50 +0200

    Update gradient col display conditions

--- hazelcast-page.php
+++ hazelcast-page.php
@@ -114,8 +114,8 @@ get_header('hazelcast'); ?>
   <div class="row justify-content-center hazelcast-gradient" id="gradient-footer">
     <div class="container full-width-product-view">
       <div class="row justify-content-center">
-        <div class="col-2 d-none d-md-block"></div>
-        <div class="col-8 py-5">
+        <div class="col-2 d-none d-sm-block"></div>
+        <div class="col py-5">
           <h1>Streamline Your Hazelcast Development Today</h1>
           <space></space>
           <p>Building highly scalable and fast applications quickly sound like an oxymoron. 
@@ -124,7 +124,7 @@ get_header('hazelcast'); ?>
           <space></space>
           <a href="http://speedment.com/hazelcast-initializer" class="btn btn-secondary">Try for Free</a>
           </div>
-          <div class="col-2 d-none d-md-block"></div>
+          <div class="col-2 d-none d-sm-block"></div>
         </div>
       </div>
     </div>
