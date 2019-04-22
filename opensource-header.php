5348d07f28dd4f30c08506dff7f4df7385e84b26
julgus <gustafsson.julia@live.se>
2019-04-22 12:02:05 +0200

    Upload open source header

---
+++ opensource-header.php
@@ -0,0 +1,24 @@
+<?php get_header('base'); ?>
+
+  <!--
+      Main Hero (#1 Tool to Accelerate Existing Database Applications)
+  -->
+  <div class="row jumbotron jumbotron-fluid" id="product-view" style="background-image: url('http://35.232.42.240/wp-content/uploads/2019/04/hazelcast-header.png');">
+    <div class="col">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col px-5" id="product-view-text-wrapper">
+            <h1>Hazelcast Auto Database Integration</h1>
+            <h3>The industry's most efficient database integration tool for data grids.</h3>
+              <div class="row">
+                <div class="col px-3" id="product-view-button-area">
+                  <a href="https://speedment.github.io/speedment-doc/introduction.html#top" target="_blank" class="btn btn-secondary">Documentation </a>
+                  <a href="http://github.com/speedment/speedment" class="btn btn-secondary">View on GitHu <i class="fab fa-github-alt"></i></a>
+                  <a href="http://speedment.com/hazelcast-initializer" class="btn btn-primary">Try for Free</a>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
