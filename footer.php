9a615111f5b8f1c9aace48549db3c455e94fcb91
julgus <gustafsson.julia@live.se>
2019-04-23 17:05:58 +0200

    Update footer

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
                 'theme_location' => 'footer-menu',
                 'menu'           => 'footer-menu',
                 'theme_location' => 'footer-menu',
-                'container'      => false,
+                'container'      => 'div',
                 'depth'          => 2,
                 'items_wrap'     => '%3$s',
                 'walker'         => new Footer_Menu_Walker()
