498180d5dbc054b828d48b2e68e07eb3898ed9b4
julgus <gustafsson.julia@live.se>
2019-04-24 11:04:59 +0200

    Replace page content with Hazelcast page

--- hazelcast-page.php
+++ hazelcast-page.php
@@ -10,7 +10,6 @@ get_header('hazelcast'); ?>
     <div class="container full-width-product-view">
       <div class="row">
         <div class="col-md-5">  
-          <img data-aos="fade-up" src="https://www.speedment.com/wp-content/uploads/2019/03/speedment-properties-2.png" alt="" width="90%" class="aligncenter size-full wp-image-1470" />
         </div>
         <div data-aos="fade-up" class="col-md-5">     
             <h1> Write Database Applications While Remaining in a Pure Java World</h1>
@@ -19,6 +18,10 @@ get_header('hazelcast'); ?>
       </div>
     </div> 
   </div>
+
+  <div class="container" id="divider">
+  </div> 
+
 
   <div class="row justify-content-center" id="compatability">
     <div class="container">
@@ -31,31 +34,7 @@ get_header('hazelcast'); ?>
     </div> 
 </div>
 
-<div class="row justify-content-center" id="datastore-">
-    <div class="container full-width-product-view">
-      <div class="row justify-content-center">
-        <div class="col-md mx-3 my-3 px-4 lightgray">
-          <div class="col">
-            <h3>Get Free Trial</h3>
-            <p>This whitepaper addresses the performance challenges for existing slow databases and presents a more modern solution – Speedment, a Stream ORM Java Toolkit and Runtime with extreme capabilities using an in JVM-memory data store. </p>
-          </div>
-          <div class="col center-button">
-            <a href="https://speedment.com/wp-content/uploads/2017/03/Speedment-White-Paper_2017.pdf" target="_blank" class="btn btn-secondary">Read Paper</a>
-          </div>
-        </div>
-        <div class="col-md mx-3 my-3 px-4 lightgray">
-          <div class="col">
-            <h3>Documentation</h3>
-            <p>Building highly scalable and fast applications quickly sound like an oxymoron. 
-              And if we throw in the possibility of doing it for your current database and application, it sounds even more like a daydream. 
-              But combining two technology stacks that share the goal of great UX through awesome DX we can achieve the impossible.</p>
-          </div>
-          <div class="col center-button">
-            <a href="https://www.youtube.com/watch?v=Ive1fTTxUpU" target="_blank" class="btn btn-secondary">Watch Now</a>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div class="container" id="divider">
   </div>
 
   <div class="row justify-content-center" id="datastore-extras">
@@ -65,6 +44,20 @@ get_header('hazelcast'); ?>
           <h1 style="text-align: center">Related Resources</h1>
         </div>
         <div class="row">
+<div class="col-md mx-3 my-3 px-4 gray">
+          <div class="col title-area"> 
+            Datasheet
+          </div>
+          <div class="col min-height">
+            <h3>Hazelcast Auto Database Integration</h3>
+            <p>Speedment offers a handy productivity tool that automates database integration with Hazelcast IMDG. 
+              It streamlines the development of Hazelcast applications by generating a Java domain model representation 
+              (POJOs and more) of the database, allowing companies to be productive with Hazelcast in no time.</p>
+          </div>
+          <div class="col center-button">
+            <a href="http://35.232.42.240/wp-content/uploads/2019/04/Hazelcast_Speedment_Datasheet.pdf" target="_blank" class="btn btn-secondary">Read Now</a>
+          </div>
+        </div>
         <div class="col-md mx-3 my-3 px-4 gray">
           <div class="col title-area"> 
             Article
@@ -77,7 +70,7 @@ get_header('hazelcast'); ?>
             <a href="http://www.javamagazine.mozaicreader.com/MayJune2017/Default/34/0#&pageSet=34&page=34" target="_blank" class="btn btn-secondary">Read Article</a>
           </div>
         </div>
-        <div class="col-md mx-3 my-3 px-4 gray">
+        <div class="col-md mx-3 my-3 px-4 gray d-md-none d-lg-block">
           <div class="col title-area"> 
             White Paper
           </div>
@@ -88,29 +81,16 @@ get_header('hazelcast'); ?>
           <div class="col center-button">
             <a href="https://speedment.com/wp-content/uploads/2017/03/Speedment-White-Paper_2017.pdf" target="_blank" class="btn btn-secondary">Read Paper</a>
           </div>
-        </div>
-        <div class="col-md mx-3 my-3 px-4 gray d-md-none d-lg-block">
-          <div class="col title-area"> 
-            Webinar
           </div>
-          <div class="col min-height">
-            <h3>Building Super Fast Data-Driven Apps with Speedment and Vaadin</h3>
-            <p>Building highly scalable and fast applications quickly sound like an oxymoron. 
-              And if we throw in the possibility of doing it for your current database and application, it sounds even more like a daydream. 
-              But combining two technology stacks that share the goal of great UX through awesome DX we can achieve the impossible.</p>
-          </div>
-          <div class="col center-button">
-            <a href="https://www.youtube.com/watch?v=Ive1fTTxUpU" target="_blank" class="btn btn-secondary">Watch Now</a>
-          </div>
-        </div>
         </div>
       </div>
     </div>
   </div>
 
-<div id="divider"></div>
+  <div class="container" id="divider">
+  </div>
 
-  <div class="row justify-content-center datastore-gradient" id="gradient-footer">
+  <div class="row justify-content-center hazelcast-gradient" id="gradient-footer">
     <div class="container full-width-product-view">
       <div class="row justify-content-center">
         <div class="col-2 d-none d-sm-block"></div>
@@ -128,7 +108,7 @@ get_header('hazelcast'); ?>
       </div>
     </div>
   </div>
-
+  
 <script type="text/javascript">
   AOS.init({
     duration: 1200,
@@ -137,4 +117,4 @@ get_header('hazelcast'); ?>
 
 <!-- End Page Content -->
 <?php get_footer(); ?>
-
+ 
