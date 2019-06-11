42224d5b9469afa3eb7feac2870a8b8f5ef65193
julgus <gustafsson.julia@live.se>
2019-06-11 13:40:19 +0200

    Fix broken link to Terms of Use

--- footer.php
+++ footer.php
@@ -42,7 +42,7 @@
         <div class="links">
           <a href="https://speedment.com/privacy" alt="Read the Speedment Privacy Policy">
             Privacy&nbsp;Policy
-          </a>&nbsp;|&nbsp;<a href="<?php echo get_permalink(172); ?>" alt="Read the Speedment Terms of Use">
+          </a>&nbsp;|&nbsp;<a href="https://speedment.com/terms" alt="Read the Speedment Terms of Use">
             Terms
           </a>
         </div>
