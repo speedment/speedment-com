e0a2eb5b03c4d635ef45baf7c82713df58bd5aa6
julgus <gustafsson.julia@live.se>
2019-06-13 16:55:04 +0200

    Upload header tools page

---
+++ header-tools.php
@@ -0,0 +1,18 @@
+<?php get_header('base'); ?>
+
+  <!--
+      Main Hero (#1 Tool to Accelerate Existing Database Applications)
+  -->
+  <div class="row jumbotron jumbotron-fluid" id="product-view" style="background-image: url('https://speedment.com/wp-content/uploads/2019/04/speedment_header.jpeg');">
+    <div class="col">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col px-5" id="product-view-text-wrapper">
+            <h1>Speedment Tools</h1>
+            <h3>Designed for commercial deployments where security, availability, and performance cannot be compromized.</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
