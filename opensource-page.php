dde29ac04adab6d3fd56bd5c331e5725888c6640
julgus <gustafsson.julia@live.se>
2019-06-13 16:13:30 +0200

    Move feature titles

--- opensource-page.php
+++ opensource-page.php
@@ -13,12 +13,16 @@ get_header('opensource'); ?>
   <div class="row justify-content-center oss-feature-gradient" id="oss-features">
     <div class="container full-width-product-view">
       <div class="row justify-content-center">
+        <div class="col">
+          <h1>View Database Tables as Standard Java Streams</h1>
+        </div>
+      </div>
+      <div class="row justify-content-center">
           <div class="col-md-1"></div>
           <div class="col-md-5 px-5 py-5">  
             <img data-aos="fade-right" src="http://speedment.com/wp-content/uploads/2019/04/stream-api-oss.png" alt="" width="100%" class="aligncenter size-full wp-image-1470" />
           </div>
           <div data-aos="fade-left" class="col-md-5 px-5 py-5">     
-              <h1>View Database Tables as Standard Java Streams</h1>
             <p></i><b> Pure Java </b> - Stream API instead of SQL eliminates the need of a query language</p>
             <p><b>Dynamic Joins </b> - Ability to perform joins as Java streams on the application side</p>
             <p>Parallel Streams</b> - Workload can automatically be divided over several threads</p>
@@ -31,9 +35,13 @@ get_header('opensource'); ?>
   <div class="row justify-content-center oss-feature-gradient" id="oss-features">
     <div class="container full-width-product-view">
       <div class="row justify-content-center">
+        <div class="col">
+          <h1>Short and Concise Type Safe Code</h1>
+        </div>
+      </div>
+      <div class="row justify-content-center">
           <div class="col-md-1"></div>
           <div data-aos="fade-right" class="col-md-5 px-5 py-5">     
-              <h1>Short and Concise Type Safe Code</h1>
             <p><b>Code Generation</b> - Automatic Java representation of the latest state of your database eliminates boilerplate code and the need of manually writing Java Entity classes while minimizing the risk for bugs</p>
             <p><b>Null Protection</b> - Minimizes the risk involved with database null values by wrapping to Java Optionals</p>
         <p><b>Enum Integration</b> - Mapping of String columns to Java Enums increases memory efficiency and type safety</p>
@@ -50,12 +58,16 @@ get_header('opensource'); ?>
   <div class="row justify-content-center oss-feature-gradient" id="oss-features">
     <div class="container full-width-product-view">
       <div class="row justify-content-center">
+        <div class="col">
+          <h1>Lazy Evaluation for Increased Performance</h1>
+        </div>
+      </div>
+      <div class="row justify-content-center">
           <div class="col-md-1"></div>
           <div class="col-md-5 px-5 py-5">  
             <img data-aos="fade-right" src="http://speedment.com/wp-content/uploads/2019/04/lazy-evaluation-oss.png" alt="" width="100%" class="aligncenter size-full wp-image-1470" />
           </div>
           <div data-aos="fade-left" class="col-md-5 px-5 py-5">     
-              <h1>Lazy Evaluation for Increased Performance</h1>
             <p><b>Streams are Lazy</b>- Content from the database is pulled as elements are needed and consumed</p>
             <p><b>Pipeline Introspection</b> - Optimized performance by short circuiting of stream operations</p>
           </div>
