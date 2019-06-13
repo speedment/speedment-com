e2bd733012f224329e9170521fadd6430546e5fb
julgus <gustafsson.julia@live.se>
2019-06-13 13:33:47 +0200

    Upload Stream header file

---
+++ header-stream.php
@@ -0,0 +1,24 @@
+<?php get_header('base'); ?>
+
+  <!--
+      Main Hero (#1 Tool to Accelerate Existing Database Applications)
+  -->
+  <div class="row jumbotron jumbotron-fluid" id="product-view" style="background-image: url('https://speedment.com/wp-content/uploads/2019/04/speedment_header.jpeg');">
+    <div class="col">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col px-5" id="product-view-text-wrapper">
+            <h1>Stream</h1>
+            <h3>Designed for commercial deployments where security, availability, and performance cannot be compromized.</h3>
+              <div class="row">
+                <div class="col px-3" id="product-view-button-area">
+                  <a href="https://speedment.github.io/speedment-doc/introduction.html" target="_blank" class="btn btn-secondary">Documentation </a>
+                  <a href="http://speedment.com/initializer" class="btn btn-primary">Try for Free</a>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
