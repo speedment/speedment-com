dd21710cf84eb20fd5f4684334118432de07129b
Julia Gustafsson <gustafsson.julia@live.se>
2019-08-02 16:00:21 +0200

    Clarify DataStore pop-over in Initializer

--- trial-page-free.php
+++ trial-page-free.php
@@ -92,7 +92,7 @@ get_header(); ?>
           <div class="form-group row">
            <legend class="col-form-legend col-sm-4">DataStore
                 <a style="color: gray" href="#" data-toggle="popover" data-trigger="hover"
-                data-placement="bottom" data-content="Enable DataStore to leverage in-memory acceleration">
+                data-placement="bottom" data-content="Enable DataStore to leverage HyperStream's in-JVM-memory capabilities for extreme performance">
                 <i class="fa fa-question-circle"></i>
             </a></legend>
             <div class="col-sm-8" id="inMemoryContainer">
