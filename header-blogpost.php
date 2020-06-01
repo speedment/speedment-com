22aec6797f3708dec8a95ef2402db8735d715086
Julia Gustafsson <gustafsson.julia@live.se>
2020-06-01 14:50:57 +0200

    Add blogpost header

---
+++ header-blogpost.php
@@ -0,0 +1,26 @@
+<?php get_header('base'); ?>
+
+<!--
+   Blogpost header
+-->
+
+<div class="row jumbotron jumbotron-fluid" id="product-view" style="background-image: url('https://speedment.com/wp-content/uploads/2019/08/extspeeder-header.jpg');">
+    <div class="col">
+        <div class="container hazelcast">
+            <div class="row justify-content-center py-3">
+                <div class="col pt-5 px-5" id="product-view-text-wrapper">
+                    <h1>Sencha Ext Speeder</h1>
+                        <h3>Build a Superfast Sencha Ext JS Grid Back-end in Minutes.</h3>
+                    <div class="row">
+                        <div class="col py-3" id="product-view-button-area">
+                            <a href="http://speedment.com/contact" class="btn btn-primary">Contact Sales</a>
+                            <a href="https://speedment.com/wp-content/uploads/2019/08/Ext-Speeder-Users-Guide.pdf" target="_blank" class="btn btn-secondary-white ml-0 ml-sm-2 mt-3 mt-sm-0">Documentation</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</header>
