3a33031c0af30c8fc05fe51112fe9b587bf0992f
Julia Gustafsson <gustafsson.julia@live.se>
2020-06-01 15:53:15 +0200

    Update blog.php

--- blog.php
+++ blog.php
@@ -3,9 +3,21 @@
 Template Name: Blog
 */
 get_header(); ?>
+
+<?php query_posts('post_type=post&post_status=publish&posts_per_page=10&paged='. get_query_var('paged')); ?>
+
+<?php if( have_posts() ): ?>
+
 <!-- Start Page Content -->
-<?php while (have_posts()) : the_post();
-  the_content();
-endwhile; ?>
+    <?php while (have_posts()) : the_post();
+        the_content();
+    endwhile; ?>
+
+    <div class="navigation">
+        <span class="newer"><?php previous_posts_link(__('« Newer','example')) ?></span> <span class="older"><?php next_posts_link(__('Older »','example')) ?></span>
+    </div><!-- /.navigation -->
+
+<?php endif; wp_reset_query(); ?>
+
 <!-- Start Page Content -->
 <?php get_footer(); ?>
