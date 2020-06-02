da204f2f5c29f65319b100c16bb08e76809d8b01
Julia Gustafsson <gustafsson.julia@live.se>
2020-06-02 14:26:39 +0200

    Update single.php

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -46,9 +46,9 @@
 	</div><!-- .entry-content -->
 
 
-	<footer class="entry-footer">
-		<?php twentynineteen_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+<!--	<footer class="entry-footer">
+		<?php /*twentynineteen_entry_footer(); */?>
+	</footer> .entry-footer -->-->
 
 	<?php if ( ! is_singular( 'attachment' ) ) : ?>
 		<?php get_template_part( 'template-parts/post/author', 'bio' ); ?>
