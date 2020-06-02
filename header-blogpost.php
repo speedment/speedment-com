09e551430f7f4321aa6903aa2dc0887fd3010790
Julia Gustafsson <gustafsson.julia@live.se>
2020-06-02 13:36:05 +0200

    Update header-blogpost

--- header-blogpost.php
+++ header-blogpost.php
@@ -6,7 +6,7 @@
 <div class="row jumbotron jumbotron-fluid justify-content-md-center" id="subpage-hero">
     <div class="col">
         <div class="container">
-            <div class="row justify-content-md-left">
+            <div class="row justify-content-md-center">
                 <div class="col col-sm-auto" id="first-view-text-wrapper">
                     <div class="col-md-auto blogpost-title justify-content-center text-centered">
                         <h1><?php if (is_404()) {echo '404 Page Not found';} else {single_post_title();} ?></h1>
@@ -14,7 +14,7 @@
                                 <?php echo get_avatar( get_the_author_meta('ID', get_post_field( 'post_author', get_queried_object_id())), 70); ?>
                             </div>
                             <p>by <?php echo get_the_author_meta('display_name', get_post_field( 'post_author', get_queried_object_id())); ?></p>
-                            <p>on <?php echo get_post_field( 'post_date', get_queried_object_id()); ?></p>
+                            <p>on <?php echo get_the_date( 'F j, Y', get_queried_object_id()); ?></p>
                     </div>
                 </div>
             </div>
