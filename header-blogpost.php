176a0bce2081ddee60fc31af208a0b58056f2e88
Julia Gustafsson <gustafsson.julia@live.se>
2020-06-02 11:39:13 +0200

    Update header-blogpost

--- header-blogpost.php
+++ header-blogpost.php
@@ -8,13 +8,13 @@
         <div class="container">
             <div class="row justify-content-md-left">
                 <div class="col col-sm-auto">
-                    <div class="col-md-auto blogpost-title justify-content-center" id="first-view-text-wrapper">
+                    <div class="col-md-auto blogpost-title justify-content-center text-centered" id="first-view-text-wrapper">
                         <h2><?php if (is_404()) {echo '404 Page Not found';} else {single_post_title();} ?></h2>
-                        <p>This article was written by <?php get_the_author(get_the_ID()); ?></p>
                         <div class="line-divider-left"></div>
                             <div id="author_pic">
-                                <?php echo get_avatar( get_the_author_meta('ID', get_post_field( 'post_author', get_queried_object_id())), 50); ?>
+                                <?php echo get_avatar( get_the_author_meta('ID', get_post_field( 'post_author', get_queried_object_id())), 70); ?>
                             </div>
+                            <p>by <?php get_post_field( 'post_author', get_queried_object_id()); ?></p>
                     </div>
                 </div>
             </div>
