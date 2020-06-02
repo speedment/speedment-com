876ec61027dbb7d0d739a9dd23a5e7d4ecd66f5f
Julia Gustafsson <gustafsson.julia@live.se>
2020-06-02 13:47:30 +0200

    Update single.php

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -12,11 +12,12 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if ( ! can_show_post_thumbnail() ) : ?>
+
+<!--	<?php /*if ( ! can_show_post_thumbnail() ) : */?>
 	<header class="entry-header">
-		<?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
+		<?php /*get_template_part( 'template-parts/header/entry', 'header' ); */?>
 	</header>
-	<?php endif; ?>
+	--><?php /*endif; */?>
 
 	<div class="entry-content">
 		<?php
