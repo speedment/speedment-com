7ef156649e1807fdff85673a0e97ab75b59718e1
Julia Gustafsson <gustafsson.julia@live.se>
2020-06-02 10:42:53 +0200

    Update header-blogpost

--- header-blogpost.php
+++ header-blogpost.php
@@ -10,7 +10,7 @@
                 <div class="col col-sm-auto">
                     <div class="col-md-auto blogpost-title justify-content-center" id="first-view-text-wrapper">
                         <h2><?php if (is_404()) {echo '404 Page Not found';} else {single_post_title();} ?></h2>
-                        <p>This article was written by <?php the_author(); ?></p>
+                        <p>This article was written by <?php get_the_author(); ?></p>
                         <div class="line-divider-left"></div>
                     </div>
                 </div>
