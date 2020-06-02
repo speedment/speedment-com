f50f7a74df39c8c3ce1589927e00be6c3a6bf1d8
Julia Gustafsson <gustafsson.julia@live.se>
2020-06-02 13:40:45 +0200

    Update single.php

--- single.php
+++ single.php
@@ -22,13 +22,13 @@ get_header('blogpost');
 				the_post();
 
 				get_template_part( 'template-parts/content/content', 'single' );
-
+/*
 				if ( is_singular( 'attachment' ) ) {
 					// Parent post navigation.
 					the_post_navigation(
 						array(
 							/* translators: %s: parent post link */
-							'prev_text' => sprintf( __( '<span class="meta-nav">Published in</span><span class="post-title">%s</span>', 'twentynineteen' ), '%title' ),
+							/*'prev_text' => sprintf( __( '<span class="meta-nav">Published in</span><span class="post-title">%s</span>', 'twentynineteen' ), '%title' ),
 						)
 					);
 				} elseif ( is_singular( 'post' ) ) {
@@ -44,11 +44,11 @@ get_header('blogpost');
 						)
 					);
 				}
-/*
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) {
 					comments_template();
-				}*/
+				}
+							*/
 
 			endwhile; // End of the loop.
 			?>
