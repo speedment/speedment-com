a87b4be54b046d3b277e05ad437303d7c4cc5b34
Julia Gustafsson <gustafsson.julia@live.se>
2020-06-02 13:38:52 +0200

    Update single.php

--- single.php
+++ single.php
@@ -44,11 +44,11 @@ get_header('blogpost');
 						)
 					);
 				}
-
+/*
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) {
 					comments_template();
-				}
+				}*/
 
 			endwhile; // End of the loop.
 			?>
