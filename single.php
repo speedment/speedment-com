2b1a1c80aa1b284c402bf473055bf227fd3650b4
Julia Gustafsson <gustafsson.julia@live.se>
2020-06-03 09:24:15 +0200

    Fix typo

--- single.php
+++ single.php
@@ -28,7 +28,6 @@ get_header('blogpost');
                     the_post();
 
                     get_template_part( 'template-parts/content/content', 'single' );
-        }
         /*
         //If comments are open or we have at least one comment, load up the comment template.
         if ( comments_open() || get_comments_number() ) {
